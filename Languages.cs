fdb3787243b26f5602b120d0a7d0ce1c06bfa1aa
calimpio <49698725+calimpio@users.noreply.github.com>
2020-03-22 02:28:51 -0500

    Update Languages.cs

--- Languages.cs
+++ Languages.cs
@@ -18,9 +18,46 @@ public class Languages : MonoBehaviour
     public static void SetLanguages(LanguagesTags lang){
         var textsLaguagues = GameObject.FindObjectsOfType(typeof(TextLanguage));
         foreach(TextLanguage tl in textsLaguagues){
-           if(TextTagLanguage.ContainsKey(tl.LanguageTag)){ 
-               tl.GetComponent<Text>().text = TextTagLanguage[tl.LanguageTag][lang];
-           }
+           if (TextTagLanguage.ContainsKey(tl.LanguageTag))
+            {
+                var text = TextTagLanguage[tl.LanguageTag][lang];
+
+                if (tl.GetComponent<Text>() != null)
+                {
+                    tl.GetComponent<Text>().text = text;
+                }
+                else
+                if (tl.GetComponent<TextMesh>() != null)
+                {
+                    tl.GetComponent<TextMesh>().text = text;
+                }
+                else
+                if (tl.GetComponent<TMPro.TextMeshPro>() != null)
+                {
+                    tl.GetComponent<TMPro.TextMeshPro>().text = text;
+                }
+                else
+                if (tl.GetComponent<TMPro.TextMeshProUGUI>() != null)
+                {
+                    tl.GetComponent<TMPro.TextMeshProUGUI>().text = text;
+                }
+                else
+                if (tl.GetComponentInChildren<Text>() != null)
+                {
+                    tl.GetComponentInChildren<Text>().text = text;
+                }
+                else
+                if (tl.GetComponentInChildren<TMPro.TextMeshProUGUI>() != null)
+                {
+                    tl.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = text;
+                }                
+                else
+                if (tl.GetComponentInChildren<TMPro.TextMeshPro>() != null)
+                {
+                    tl.GetComponentInChildren<TMPro.TextMeshPro>().text = text;
+                }
+            }
+            }
         }
     }
 
